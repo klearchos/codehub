d3a33348f9bc7c19f4ac7a1983ca49e8168e8ee7
kklearchou@gmail.com <kklearchou@gmail.com>
2019-05-28 21:57:10 +0300

    Problem description for OOD.

---
+++ src/main/java/ood/Application.java
@@ -0,0 +1,13 @@
+package ood;
+
+public class Application {
+
+  private static final String TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+  public static void main(String[] args) {
+    new Application();
+  }
+
+  Application() {
+  }
+}
